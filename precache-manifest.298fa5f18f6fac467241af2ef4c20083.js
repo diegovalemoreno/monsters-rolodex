66c3388fcf7b0469a1566ecd5205a231ed05a7a7
diegovalemoreno <diegosvm@hotmail.com>
2019-07-16 18:37:18 -0300

    Updates

---
+++ precache-manifest.298fa5f18f6fac467241af2ef4c20083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427b95c0efed2abba1b44f49cdecf801",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26599df0b96b1a723ac5",
+    "url": "/monsters-rolodex/static/css/main.3ec2bdd9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "26599df0b96b1a723ac5",
+    "url": "/monsters-rolodex/static/js/main.f9e7cd40.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
